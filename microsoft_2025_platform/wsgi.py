9553f83ff0ce254c3652fe3d2e4ab865612b0852
JoelCaquene <joelcaquenejr@gmail.com>
2025-08-09 04:26:57 -0300

    feat: Adiciona WhiteNoise para servir arquivos estáticos em produção

--- microsoft_2025_platform/wsgi.py
+++ microsoft_2025_platform/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise  # Importe o WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'microsoft_2025_platform.settings')
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application) # Aplica o WhiteNoise à sua aplicação
