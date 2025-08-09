fd6ffe277af9eeb38239c73538dfe3242a2e3d5d
JoelCaquene <joelcaquenejr@gmail.com>
2025-08-09 03:40:54 -0300

    Correção de build.sh para incluir collectstatic e migrate

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
+
 # Exit on error
 set -o errexit
 
-# Install dependencies (opcional, pode ser removido se já estiver no render.yaml)
+# Install dependencies from requirements.txt
 pip install -r requirements.txt
-
-# Run migrations (opcional, já configurado para rodar como um worker no render.yaml)
-# python manage.py migrate
 
 # Collect static files
 python manage.py collectstatic --noinput
+
+# Run database migrations
+python manage.py migrate
